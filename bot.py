fa95f80ecdaed0f31f980d649a023d503a4c0bf4
dawidziel <dawidziel101@gmail.com>
2022-05-19 22:20:50 +0200

    fixed bug

--- bot.py
+++ bot.py
@@ -43,16 +43,15 @@ def soloq(day_nr):
 
     # Returning list of games for every player in the time period between 1 to 365
     for id in team_id:
-        player_mh = []
-        player_mh.extend(watcher.match.matchlist_by_puuid(region, id, queue=420, start_time=start_time, end_time=current_time, count=100))
+        player_mh = list(watcher.match.matchlist_by_puuid(region, id, queue=420, start_time=start_time, end_time=current_time, count=100))
 
-        count = 0   #will be used to make sure loop wont stuck if player played 100 games exactly
         while len(player_mh) % 100 == 0 and len(player_mh) > 0:
+            old_count = len(player_mh)
             last_id = player_mh[-1]
             last_date = str(watcher.match.by_id(my_region,last_id)['info']['gameCreation'])[:10]
             player_mh.extend(watcher.match.matchlist_by_puuid(region, id, queue=420, start_time=start_time, end_time=int(last_date), count=100))
 
-            if count > 0 and len(player_mh)==100:
+            if old_count == player_mh:
                 break
             count = count + 1
         team_mh.append(player_mh)
