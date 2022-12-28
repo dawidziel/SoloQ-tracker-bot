dd4ab8b7f17adfc2bf78182b1b32e2af1c619805
dawidziel <dawidziel101@gmail.com>
2022-12-29 00:58:47 +0100

    changed lolpros scraping method

--- lol_pros_accounts.py
+++ lol_pros_accounts.py
@@ -1,18 +1,21 @@
-import json
-import urllib.request
+from mechanize import Browser
+from bs4 import BeautifulSoup
 
 class LolProsAcc:
     def accountsByPlayers(player_names):
         accounts = []
-        player_names = [x.replace(" ","-") for x in player_names]
+        for player_name in player_names:
+            b = Browser()
+            b.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+            b.open('https://lolpros.gg/player/'+player_name)
+            soup = BeautifulSoup(b.response().read(), "html.parser")
+            button_names = soup.find_all("button", {"class": "account"})
 
-        for playername in player_names:
-            url = 'https://api.lolpros.gg/es/players/'
+            if button_names != []:
+                accounts.extend([(player_name, x['aria-label']) for x in button_names])
+            else:
+                image_name = ' '.join(soup.find("div", {"class": "summoner-name"}).text.split(' ')[:-1])
+                accounts.append((player_name, image_name))
+            b.close()
 
-            with urllib.request.urlopen(url+playername) as url:
-                index = json.loads(url.read().decode())
-
-            for y in index['league_player']['accounts']:
-                accounts.append((index['name'],y['summoner_name']))
-                
         return accounts
